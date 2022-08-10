9a5d724ceb399898085349e1b3c2196bce20f13f
JJH <3198636900@qq.com>
2022-08-10 20:31:27 +0800

    引入uview

--- App.vue
+++ App.vue
@@ -15,6 +15,7 @@
 
 <style lang="scss">
 	/*每个页面公共css */
+	@import "@/uni_modules/uview-ui/index.scss";
 	@import '@/uni_modules/uni-scss/index.scss';
 	/* #ifndef APP-NVUE */
 	@import '@/static/customicons.css';
--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import uView from "./uni_modules/uview-ui";
+Vue.use(uView);
 
 Vue.config.productionTip = false
 
