6fd4a9bcc997b30585879d55b08c55bbf57b247a
full_sinm <1747373447@qq.com>
2022-08-11 11:58:53 +0800

    注册登陆页面纯静态

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,9 @@
 <template>
 	<view class="container">
 		
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<uni-link :href="href" :text="href"></uni-link>
+		
+		<button @click="topage_log">点击登录</button>
+		<button @click="topage_reg">点击注册</button>
 	</view>
 </template>
 
@@ -11,11 +11,20 @@
 	export default {
 		data() {
 			return {
-				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+				
 			}
 		},
 		methods: {
-
+			topage_log() {
+				uni.navigateTo({
+					url:"../log/log"
+				})
+			},
+			topage_reg() {
+				uni.navigateTo({
+					url:"../reg/reg"
+				})
+			}
 		}
 	}
 </script>
---
+++ pages/log/log.vue
@@ -0,0 +1,131 @@
+<template>
+	<view class="">
+		
+		<!-- 空白部分 -->
+		<view class="blank">
+
+		</view>
+			
+		<!-- 主体 -->
+		<view class="uni-form-item uni-column horViewStyle box">	
+			<image src="../../static/log1.svg" class="picture" mode=""></image>
+			<input class="uni-input input-box" type="number" placeholder="请输入手机号" :value="iphoneValue" />
+		</view>
+				
+		<view class="uni-form-item uni-column horViewStyle box">
+			<image src="../../static/log2.svg" class="picture" mode=""></image>
+			<input class="uni-input input-box" type="password" placeholder="请输入密码" v-model="passwordValue"/>
+		</view>
+		
+			<button class="login-btn" >登录</button>
+
+		
+					
+	</view>
+</template>
+
+<script>
+	export default {
+	    data() {
+	        return {
+				iphoneValue: '', //手机号码
+				passwordValue: '',//密码
+		}
+	    },
+	    methods: {
+			// Login(){
+			// 	uni.request({
+			// 		let that = this
+			// 		url: 'http://app/login', // 路径
+			// 		method: 'POST', // 请求方法
+			// 		data: {
+			// 			phone: that.iphoneValue,
+			// 			password: that.passwordValue
+			// 		}, //发送的数据
+			// 		success: (res) => {
+			// 			if (res.data.code == 200) {
+			// 				//存储token
+			// 				that.token = res.data.token;
+			// 				uni.setStorageSync('token', that.token); // 将登录信息以token的方式存在硬盘中
+			// 				uni.setStorageSync('userInfo', JSON.stringify(res.data)); // 将用户信息存储在硬盘中
+			// 				uni.switchTab({ // 跳转到新闻页面
+			// 					url: "../index/index",
+			// 				})
+			// 				uni.showToast({
+			// 					title: '登录成功',
+			// 					icon: 'none'
+			// 				})
+			// 			} else {
+			// 				uni.showToast({
+			// 					title: '登录失败',
+			// 					icon: 'none'
+			// 				})
+			// 			}
+			// 		}
+			// 	})
+			// }
+	    }
+	}
+
+</script>
+<style>
+	
+		.iphone,
+		.password,
+		.test {
+			position: relative;
+			margin-bottom: 30px;
+		}
+		.box {
+			height: 100upx;
+			width: 600upx;
+			margin-top: 10upx;
+			margin-bottom: 20upx;
+			margin-left: 70upx;
+			background-color: #f0f0f0;
+			border-radius: 36px;
+			display: flex;
+		}
+		.picture{
+			height:100%;
+			width: 50upx;
+			margin-left: 20upx;
+			border-radius: 36upx;
+		}
+        .wrap{
+            display: flex;
+            width: 100%;
+            height: 500upx;
+        }
+        .blank{
+            border: 1px  #000;
+			width:100%;
+			height:400upx;
+            flex: 1;
+        }
+		.input-box {
+			margin-top: 25upx;
+			margin-left:10upx;
+		}
+		.input-btn{
+			border-radius: 36px;
+			background-color: #000;
+			height: 100upx;
+			width: 600upx;
+			margin-left: 80upx;
+		}
+		.login-btn {
+			width: 80%;
+			height: 100rpx;
+			background: #000000;
+			border-radius: 36px;
+			color: #ffffff;
+			font-size: 20px;
+			text-align: center;
+			line-height: 45px;
+			position: absolute;
+			margin-bottom: 500rpx;
+			margin-left: 70rpx;
+			margin-right: 70rpx;
+		}
+ </style>
