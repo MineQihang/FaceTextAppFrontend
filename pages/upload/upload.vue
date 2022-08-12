6b50c62498296be898ce47dce6b43c1be5883fa0
JJH <3198636900@qq.com>
2022-08-12 19:55:23 +0800

    发布页面。。。正在做

--- pages/upload/upload.vue
+++ pages/upload/upload.vue
@@ -1,6 +1,10 @@
 <template>
-	<view>
-		这是上传页面
+	<view class="container">
+		<uni-icons type="arrow-left" size="40" class="back-icon" @click="back()"></uni-icons>
+		<view class="text_con">
+			<textarea class="text" cols="30" rows="10" v-model="text"></textarea>
+		</view>
+		
 	</view>
 </template>
 
@@ -8,15 +12,38 @@
 	export default {
 		data() {
 			return {
-				
+				text: ""
 			}
 		},
 		methods: {
-			
+			back(){
+				uni.navigateBack();
+			}
 		}
 	}
 </script>
 
 <style>
+	.container {
 
+	}
+	.back-icon {
+		position: fixed;
+		top: 26rpx;
+		left: 8rpx;
+	}
+	.text_con {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.text {
+		width: 337px;
+		height: 230px;
+		top: 92px;
+		background: rgba(196, 196, 196, 0.14);
+		border-radius: 16px;
+	}
 </style>
