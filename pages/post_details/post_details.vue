ef5c04074f383b58a2d9d0a2384fcc744a9b2b6e
full_sinm <1747373447@qq.com>
2022-08-14 18:09:20 +0800

    可以看贴了 但评论还看不到

--- pages/post_details/post_details.vue
+++ pages/post_details/post_details.vue
@@ -90,7 +90,8 @@
 	export default {
 		onLoad: function(option) { //option为object类型，会序列化上个页面传递的参数
 			console.log(option.pid);
-			this.pid = parseInt(option.pid); //打印出上个页面传递的参数。//打印出上个页面传递的参数。
+			this.pid = parseInt(option.pid);
+			console.log(this.pid) //打印出上个页面传递的参数。//打印出上个页面传递的参数。
 		},
 		data() {
 			return {
@@ -103,18 +104,20 @@
 						name: 'heart-filled'
 					}
 				],
+				lon: false,
 				swipers: [
 					'/static/Header_img.png',
 					'/static/Header_img.png',
 					'/static/Header_img.png',
 				],
-				pid: '', //帖子的id
+				pid: 1660373121686, //帖子的id
 				like: 0, //是否给这篇帖子点赞了
 				icon: '/static/Header_img.png', //发帖人头像
 				username: 'ikun', //发帖用户名
 				time: '两年半之前', //发帖时间
 				numberComment: 0, //评论数
 				numberLike: 0, //点赞数
+				allComments: [], //所有评论
 				post_title: '滕王阁序', //帖子标题
 				post_main: '豫章故郡，洪都新府。星分翼轸，地接衡庐。襟三江而带五湖，控蛮荆而引瓯越。物华天宝，龙光射牛斗之墟；人杰地灵，徐孺下陈蕃之榻。雄州雾列，俊采星驰。台隍枕夷夏之交，宾主尽东南之美。都督阎公之雅望，棨戟遥临；宇文新州之懿范，襜帷暂驻。十旬休假，胜友如云；千里逢迎，高朋满座。腾蛟起凤，孟学士之词宗；紫电青霜，王将军之武库。家君作宰，路出名区；童子何知，躬逢胜饯时维九月，序属三秋。潦水尽而寒潭清，烟光凝而暮山紫。俨骖騑于上路，访风景于崇阿。临帝子之长洲，得天人之旧馆。层峦耸翠，上出重霄；飞阁流丹，下临无地。鹤汀凫渚，穷岛屿之萦回；桂殿兰宫，即冈峦之体势', //帖子正文
 			}
@@ -130,10 +133,10 @@
 						method: 'POST',
 						header: {
 							'Authorization': authorization,
-							"content-type": "multipart/form-data"
+							"content-type": "application/x-www-form-urlencoded"
 						},
 						data: {
-							pid: that.pid
+							pid: 1660471475672
 						},
 						success: (res) => {
 							console.log(res)
@@ -141,7 +144,13 @@
 							if (res.statusCode == 200) {
 								that.numberComment = res.data.data.commentNum;
 								that.numberLike = res.data.data.likeNum;
-
+								that.post_title = res.data.data.title;
+								that.post_main = res.data.data.context;
+								that.time = res.data.data.createdTime;
+								that.swipers = res.data.data.imgUrls;
+								that.allComments = res.data.data.comments;
+								that.icon = res.data.data.iconUrl;
+								that.username = res.data.data.username;
 							} else {
 								uni.showToast({
 									title: res.data.detail,
