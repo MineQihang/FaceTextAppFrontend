c1a40c15abdc69eb2bb15df62c30f02b9f933319
Mine_qihang <2206457717@qq.com>
2022-08-16 19:34:51 +0800

    更新App.vue

--- App.vue
+++ App.vue
@@ -1,44 +1,44 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.warn('当前组件仅支持 uni_modules 目录结构 ，请升级 HBuilderX 到 3.1.0 版本以上！')
-			console.log('App Launch')
-			uni.onTabBarMidButtonTap(() => {
+  export default {
+    onLaunch: function() {
+      console.warn('当前组件仅支持 uni_modules 目录结构 ，请升级 HBuilderX 到 3.1.0 版本以上！')
+      console.log('App Launch')
+      uni.onTabBarMidButtonTap(() => {
 
-				uni.navigateTo({
+        uni.navigateTo({
 
-					url: "/pages/upload/upload",
-					animationType: "slide-in-bottom",
-					animationDuration: 150
-				})
-			})
-		},
-		onShow: function() {
-			// console.log('App Show')
-		},
-		onHide: function() {
-			// console.log('App Hide')
-		}
-	}
+          url: "/pages/upload/upload",
+          animationType: "slide-in-bottom",
+          animationDuration: 150
+        })
+      })
+    },
+    onShow: function() {
+      // console.log('App Show')
+    },
+    onHide: function() {
+      // console.log('App Hide')
+    }
+  }
 </script>
 
 
 <style lang="scss">
-	/*每个页面公共css */
-	@import "@/uni_modules/uview-ui/index.scss";
-	@import '@/uni_modules/uni-scss/index.scss';
-	/* #ifndef APP-NVUE */
-	@import '@/static/customicons.css';
+  /*每个页面公共css */
+  @import "@/uni_modules/uview-ui/index.scss";
+  @import '@/uni_modules/uni-scss/index.scss';
+  /* #ifndef APP-NVUE */
+  @import '@/static/customicons.css';
 
-	// 设置整个项目的背景色
-	page {
-		background-color: #f5f5f5;
-	}
+  // 设置整个项目的背景色
+  page {
+    background-color: #f5f5f5;
+  }
 
-	/* #endif */
-	.example-info {
-		font-size: 14px;
-		color: #333;
-		padding: 10px;
-	}
+  /* #endif */
+  .example-info {
+    font-size: 14px;
+    color: #333;
+    padding: 10px;
+  }
 </style>
